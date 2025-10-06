59e46e41e1347269ed696567e882cc49f0b70d33
tameemahamed <ahamedtameem00@gmail.com>
2025-10-06 19:07:07 +0600

    added sellers table

---
+++ database/migrations/2025_10_06_101206_create_seller_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seller_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained('sellers')->noActionOnDelete();
+            $table->string('mobile_number');
+            $table->text('address_line_1');
+            $table->text('address_line_2');
+            $table->string('thana_or_upozilla');
+            $table->foreignId('district_id')->constrained('districts')->cascadeOnDelete();
+            $table->foreignId('division_id')->constrained('divisions')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seller_addresses');
+    }
+};
