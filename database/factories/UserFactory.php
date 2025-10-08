148aa67a96fcc55380685693f87278f3b439854a
tameemahamed <ahamedtameem00@gmail.com>
2025-10-08 15:40:31 +0600

    updated UserFactory as per the tables

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,22 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->name();
+        $mail = ['yahoo', 'gmail', 'hotmail'];
+        $username = str_replace(' ', '', strtolower($name));
+        $email = $username.rand(1,299).'@'.$mail[array_rand($mail)].'com';
+        $mobile = '+8801'.rand(3,9).rand(10000000, 99999999);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $name,
+            'username' => $username,
+            'email' => $email,
+            'mobile_number' => $mobile,
+            'gender' => rand(false, true),
+            'date_of_birth' => fake()->dateTimeBetween('1960-01-01', '2011-12-31'),
             'email_verified_at' => now(),
+            'mobile_verified_at' => now(),
+            'role_id' => 3,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
