303778a78451d0bae56ff20ba55e147e2c5e865b
tameemahamed <ahamedtameem00@gmail.com>
2025-10-08 22:11:06 +0600

    converted gender to boolean

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -35,7 +35,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('mobile_number')->unique();
-            $table->binary('gender');
+            $table->boolean('gender');
             $table->date('date_of_birth');
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('mobile_verified_at')->nullable();
